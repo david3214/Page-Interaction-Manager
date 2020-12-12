74351e97bec7b39339f7525f2b7bd1f6a7eb0f7f
grahas <gupyfish@gmail.com>
2020-12-12 04:25:24 +0000

    partialupdates

--- api/main_test.py
+++ api/main_test.py
@@ -1,10 +1,14 @@
 import pytest
+import json
 
+import main
+
+with open("/workspaces/missionary-tools/appscript/page_interaction/mock_data.json") as f:
+  mock_data = json.load(f)
 
 @pytest.fixture
 def app(monkeypatch):
     monkeypatch.setenv('REDISCLOUD_URL', '***REMOVED***')
-    import main
     main.app.testing = True
     return main.app.test_client()
 
@@ -13,3 +17,9 @@ def test_index(app):
     r = app.get('/')
     assert r.status_code == 200
 
+def test_credentials(app):
+
+    r = app.post('/page-interaction-manager/credentials', data=mock_data['page_results'])
+    assert r.status_code == 200
+    r = app.get('/page-interaction-manager/credentials?id=1')
+    assert r.status_code == 200
