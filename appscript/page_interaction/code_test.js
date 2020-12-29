09348011e80c0fb9f50ee0013b1b822fe66c0e75
grahas <gupyfish@gmail.com>
2020-12-29 08:43:22 +0000

    connect to db

--- appscript/page_interaction/code_test.js
+++ appscript/page_interaction/code_test.js
@@ -1,8 +1,10 @@
+var mode = "PRODUCTION";
+
 QUnit.helpers( this );
 function testFunctions() {
    testingFacebookWebhookUpdate();
 }
-/*
+
 function doGet( e ) {
      QUnit.urlParams( e.parameter );
      QUnit.config({
@@ -12,7 +14,7 @@ function doGet( e ) {
  
      return QUnit.getHtml();
 };
-*/
+
 function testingFacebookWebhookUpdate(){
     QUnit.test("facebook post testing", function(assert) {
         test_data.sample_page_notifications_accept.forEach(function(data){
@@ -63,23 +65,54 @@ function testingFacebookWebhookUpdate(){
         function test_doLogicPageMessages(){
             /* Test the sheet is working */
             var sheet = SpreadsheetApp.openById("1bKbHJAUn6E41E6H-_ZdmsFViXctchO_w6SzrIaAMmas").getSheetByName("Ad Likes");
-            sheet.appendRow(["1",	"a",	"m",	"a",	"2",	"a",	"Assignmenta3",	"a",	"a",	"3",	"3",	"3",	"3"]);
+            sheet.appendRow(["1",	"tom",	"male",	"a",	"2",	"a",	"Assignment",	"a",	"a",	"👍",	"3",	"3",	"3"]);
             var e = JSON.parse('{ "authMode": "FULL", "changeType": "INSERT_ROW", "source": {}, "triggerUid": "502test6549", "user": { "email": "test.test@test.org", "nickname": "test.test" }}');
             doLogicPageMessages(e, sheet);
         }
         assert.ok(test_doLogicPageMessages, "Should see a new line get inserted");
     });
+    const _ = LodashGS.load();
     QUnit.test("Test database preferences", function(assert){
-        assert.ok(setPreference(test_data.sample_page_details_property.google_sheet.id, test_data.sample_page_details_property, "Can set preferences"));
-        assert.ok(getPreference(test_data.sample_page_details_property.google_sheet.id), "Can get preferences");
-        assert.ok(deletePreference(test_data.sample_page_details_property.google_sheet.id), "Can delete preferences")
+        assert.ok(setPreference(test_data.sample_page_details_property.google_sheets.id, test_data.sample_sheet_settings), "Can set preferences");
+        assert.equal(true, _.isEqual(getPreference(test_data.sample_page_details_property.google_sheets.id), test_data.sample_sheet_settings), 'Can get the same preferences that were set');
+        assert.ok(deletePreference(test_data.sample_page_details_property.google_sheets.id), "Can delete preferences");
     });
     QUnit.test("Test database page details", function(assert){
-        assert.ok(setPageDetail(test_data.sample_page_details_property.id, test_data.sample_page_details_property, "Can set page details"));
-        assert.ok(getPageDetail(test_data.sample_page_details_property.id), "Can get page details");
-        assert.ok(deletePageDetail(test_data.sample_page_details_property.id), "Can delete page details");
-        assert.ok(getAllPageDetail(), "Can list all page details")
+        assert.ok(setPageDetails(test_data.sample_page_details_property.id, test_data.sample_page_details_property), "Can set page details");
+        assert.equal(true, _.isEqual(getPageDetails(test_data.sample_page_details_property.id), test_data.sample_page_details_property), "Can get page details");
+        assert.ok(deletePageDetails(test_data.sample_page_details_property.id), "Can delete page details");
+        assert.ok(getAllPageDetails(), "Can list all page details");
     });
 }
 
+function test_db_pref(){
+    setPreference(test_data.sample_page_details_property.google_sheets.id, test_data.sample_sheet_settings);
+    getPreference(test_data.sample_page_details_property.google_sheets.id);
+    deletePreference(test_data.sample_page_details_property.google_sheets.id);
+}
 
+function test_db_page_details(){
+  setPageDetails(test_data.sample_page_details_property.id, test_data.sample_page_details_property);
+  getPageDetails(test_data.sample_page_details_property.id);
+  deletePageDetails(test_data.sample_page_details_property.id);
+  getAllPageDetails();
+}
+
+function test_sheet_creation(){
+    mode = "TEST";
+    var spreadSheet = SpreadsheetApp.openById("1bKbHJAUn6E41E6H-_ZdmsFViXctchO_w6SzrIaAMmas");
+    setUpSheet(spreadSheet);
+}
+
+function test_get(){
+    getPreference(test_data.sample_page_details_property.google_sheets.id);
+}
+function test_doLogicPageMessages(){
+    /* Test the sheet is working */
+    setPreference(test_data.sample_page_details_property.google_sheets.id, test_data.sample_sheet_settings);
+    var spreadSheet = SpreadsheetApp.openById("1bKbHJAUn6E41E6H-_ZdmsFViXctchO_w6SzrIaAMmas");
+    var sheet = spreadSheet.getSheetByName("Ad Likes");
+    sheet.appendRow(["1",	"tom",	"male",	"a",	"2",	"a",	"Assignment",	"a",	"a",	"👍",	"3",	"3",	"3"]);
+    var e = JSON.parse('{ "authMode": "FULL", "changeType": "INSERT_ROW", "source": {}, "triggerUid": "502test6549", "user": { "email": "test.test@test.org", "nickname": "test.test" }}');
+    doLogicPageMessages(e, spreadSheet);
+}
