995e922b0a681f37d5d07a9d068b519bfa74738a
grahas <gupyfish@gmail.com>
2021-01-27 21:58:02 +0900

    update creds

--- webserver-authentication/main.py
+++ webserver-authentication/main.py
@@ -264,6 +264,11 @@ def print_index_table():
 
 
 if __name__ == '__main__':
+  # When running locally, disable OAuthlib's HTTPs verification.
+  # ACTION ITEM for developers:
+  #     When running in production *do not* leave this option enabled.
+  #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.
   app.run('localhost', 8080, debug=True)
