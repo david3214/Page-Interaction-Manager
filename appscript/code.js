1cb32cd23103e24064719279dbc1891de850a29a
grahas <gupyfish@gmail.com>
2021-02-20 15:28:17 +0900

    updoots

--- appscript/code.js
+++ appscript/code.js
@@ -6,23 +6,33 @@ var baseURL = "https://api-dot-eighth-vehicle-287322.uc.r.appspot.com/"; // Don'
 const _ = LodashGS.load();
 
 function onOpen(e) {
-    SpreadsheetApp.getUi() 
-      .createMenu('Missionary Tools')
-      .addSubMenu(SpreadsheetApp.getUi().createMenu('Find member profiles')
-            .addItem('Start', 'showPopup')
-            .addItem('Reset', 'reset'))
-      .addSeparator()
-      .addSubMenu(SpreadsheetApp.getUi().createMenu('Page interaction manager')
-            .addItem('Create', 'setUpSheet')
-            .addItem('Analytics', 'showAnalytics')
-            .addItem('Debug', 'showDebugSidebar')
-            .addItem('Settings', 'showSettings'))
-      .addToUi();
-    /*
-      if (e && e.authMode != ScriptApp.AuthMode.NONE) {
-        addUserToDB();
+  var menu = SpreadsheetApp.getUi().createMenu('Missionary Tools')
+  .addSubMenu(SpreadsheetApp.getUi().createMenu('Find member profiles')
+        .addItem('Start', 'showPopup')
+        .addItem('Reset', 'reset'))
+  .addSeparator()
+  .addSubMenu(SpreadsheetApp.getUi().createMenu('Page interaction manager')
+  .addItem('Create', 'setUpSheet')
+  .addItem('Analytics', 'showAnalytics')
+  .addItem('Format', 'formatSheet')
+  .addItem('Settings', 'showSettings'))
+/**
+  if (e && e.authMode != ScriptApp.AuthMode.NONE) { //
+    // Add a menu item based on properties (doesn't work in AuthMode.NONE).
+    Logger.log(e.authMode)
+    Logger.log(Session.getActiveUser().getEmail())
+    
+    
+    var debugUsers = JSON.parse(PropertiesService.getScriptProperties().getProperty("debugEnabled")).users;
+    if (_.includes(debugUsers, Session.getActiveUser().getEmail())) {
+      // add debug menu
+      menu.addItem('Debug', 'showDebugSidebar')
     }
-    */
+  }
+  */
+  menu.addToUi();
+
+    
 }
 
 // Make the menu appear after installing it
