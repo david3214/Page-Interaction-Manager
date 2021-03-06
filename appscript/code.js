7c439ce5e75cfdb249d3c9fcbf78c86e5b09815e
grahas <gupyfish@gmail.com>
2021-03-07 00:36:59 +0900

    updates bug fix

--- appscript/code.js
+++ appscript/code.js
@@ -12,29 +12,12 @@ function onOpen(e) {
   .addItem('Analytics', 'showAnalytics')
   .addItem('Format', 'formatSheet')
   .addItem('Settings', 'showSettings'))
-/**
-  if (e && e.authMode != ScriptApp.AuthMode.NONE) { //
-    // Add a menu item based on properties (doesn't work in AuthMode.NONE).
-    Logger.log(e.authMode)
-    Logger.log(Session.getActiveUser().getEmail())
-    
-    
-    var debugUsers = JSON.parse(PropertiesService.getScriptProperties().getProperty("debugEnabled")).users;
-    if (_.includes(debugUsers, Session.getActiveUser().getEmail())) {
-      // add debug menu
-      menu.addItem('Debug', 'showDebugSidebar')
-    }
-  }
-  */
   menu.addToUi();
-
-    
 }
 
 // Make the menu appear after installing it
 function onInstall(e) {
     onOpen(e);
-    // Perform additional setup as needed.
     addUserToDB();
 }
 
