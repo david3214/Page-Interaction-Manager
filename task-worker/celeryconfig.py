42f22c57d149f29ad058a54cda7f025cdc324169
grahas <gupyfish@gmail.com>
2021-03-14 20:35:38 +0900

    restructuring

--- task-worker/celeryconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-
-## Broker settings.
-broker_url = os.getenv("RABBITMQ_URL")
-
-result_backend = "rpc://"
-
-# List of modules to import when the Celery worker starts.
-imports = ('tasks',)
