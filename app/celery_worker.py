76ef483f701fc2428ed327f3e2ca66285381b13b
david3214 <davidw3214@gmail.com>
2021-07-12 20:27:25 -0500

    Updated Kubernetes yamls to work, as well as updated the app dockerfile to allow for running celery as well as app

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,5 +1,14 @@
 import os
 from app import celery, create_app
+from celery.schedules import crontab
 
 foo = create_app(os.getenv('FLASK_CONFIG') or 'default')
 foo.app_context().push()
+
+celery.conf.CELERYBEAT_SCHEDULE = {
+    "run-every-ten-seconds":{
+        "task": "app.worker.tasks.update_all_profile_links",
+        "schedule": crontab(minute=0, hour='*/6'),
+        "options": {'queue': 'results'}
+    }
+}
