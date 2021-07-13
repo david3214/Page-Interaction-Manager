ea8aabd56f96df034b2084dd592158918d476406
david3214 <davidw3214@gmail.com>
2021-07-13 17:50:41 -0500

    Made the profile scraper celery worker only authenticate with facebook x number of times once where x is the cpu count

--- task-worker/missionary_bot/tasks.py
+++ task-worker/missionary_bot/tasks.py
@@ -8,6 +8,7 @@ import urllib.request
 from time import sleep
 
 from celery import Celery
+from celery.signals import worker_process_init, worker_shutdown
 
 from .bot import MissionaryBot
 from .config import config
@@ -15,6 +16,24 @@ from .config import config
 celery = Celery('tasks', broker=os.getenv("RABBITMQ_URL"), backend=os.getenv("REDISCLOUD_URL"))
 #jesus_bg = Image.open(urllib.request.urlopen(
 #    "https://storage.googleapis.com/eighth-vehicle-287322.appspot.com/qr-code/jesus_template.png").read())
+
+bots = []
+
+@worker_process_init.connect
+def init_worker(**kwargs):
+    global bots
+    print('Creating {} bots'.format(cpu_count()))
+    for _ in range(cpu_count()):
+        bot = MissionaryBot(config=config['default'])
+        bot.language = os.getenv("FACEBOOK_LANGUAGE")
+        bot.authenticate_with_facebook()
+        bots.append(bot)
+        sleep(5) # Don't like but facebook don't like to multiple simultaeneous logins
+
+@worker_shutdown.connect
+def shutdown_worker(**kwargs):
+    for i in range(len(bots)):
+        bots[i].wd.quit()
 
 @celery.task(reply_to='results')
 def test_task(task_info):
@@ -30,14 +49,13 @@ def get_profile_links(task_info):
                type: get_profile_links
                results: {name:url}
     """
+    global bots
+    print('bot_list length: {}'.format(len(bots)))
     workQ = queue.Queue()
     resultsQ = queue.Queue()
-    def worker(queue):
+    def worker(queue, bot):
         global results
         try:
-            bot = MissionaryBot(config=config['default'])
-            bot.language = os.getenv("FACEBOOK_LANGUAGE")
-            bot.authenticate_with_facebook()
             while True:
                 item = queue.get()
                 print(f'Working on {item}')
@@ -49,10 +67,9 @@ def get_profile_links(task_info):
                 print(f'Finished {item}')
                 queue.task_done()
                 if queue.empty():
-                    bot.wd.quit()
                     break
         except:
-            bot.wd.quit()
+            pass
     def merge_results(queue):
         global results
         while True:
@@ -65,10 +82,8 @@ def get_profile_links(task_info):
     print('All task requests sent\n', end='')
     threading.Thread(target=merge_results, daemon=True, args=[resultsQ], name="Merge Results").start()
 
-    # for _ in range(cpu_count()):
-    for _ in range(2):
-        threading.Thread(target=worker, daemon=True, name=f"Profile_worker_{_}", args=[workQ]).start()
-        sleep(5) # Don't like but facebook don't like to multiple simultaeneous logins
+    for _ in range(len(bots)):
+        threading.Thread(target=worker, daemon=True, name=f"Profile_worker_{_}", args=[workQ, bots[_]]).start()
 
     workQ.join()
     print('All work completed')
