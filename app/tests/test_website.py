863a14ae7a975b6659adde69a408213b904ba238
david3214 <davidw3214@gmail.com>
2021-09-08 16:10:27 -0500

    Updated tests for worker, and website, as well as added static test data in test_data.json

--- app/tests/test_website.py
+++ app/tests/test_website.py
@@ -6,6 +6,7 @@ from flask import Flask
 
 import unittest
 from flask import current_app, url_for
+from flask.templating import render_template
 from app import create_app, db
 
 
@@ -25,3 +26,39 @@ class WebsiteTestCase(unittest.TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.data.find(b'Home') != -1)
+
+    def test_help_page(self):
+        response = self.client.get('/help')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data, b'Watch the video to learn how to use this program')
+
+    def test_privacy_page(self):
+        response = self.client.get('/privacy')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.data.find(b'Privacy Policy') != -1)
+
+    def test_support_page(self):
+        response = self.client.get('/support')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data, b'Email me at graham.harrison@missionary.org')
+
+    def test_post_install_tip_page(self):
+        response = self.client.get('/post-install-tip')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data, b'Click the run addon button')
+
+    def test_terms_of_service_page(self):
+        response = self.client.get('/terms-of-service')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.data.find(b'Terms and Conditions') != -1)
+
+    def test_google_verification_page(self):
+        response = self.client.get('/google46b0d5ef2ffda0c5.html')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.data.find(b'google-site-verification: google46b0d5ef2ffda0c5.html') != -1)
+
+    def test_assets_page(self):
+        response = self.client.get('/assets/test.html')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.data.find(b'Log In with the JavaScript SDK') != -1)
